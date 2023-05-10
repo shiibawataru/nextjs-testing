84e92b4746ba8650e8644f9122d68feb15bc591f
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2023-05-10 16:48:36 +0900

    initial commit

---
+++ components/Comment.tsx
@@ -0,0 +1,21 @@
+import { COMMENT } from '../types/Types'
+
+import React from 'react'
+
+const Comment: React.FC<COMMENT> = ({ id, name, body }) => {
+  return (
+    <li className="mx-10">
+      <p className="">
+        {id}
+        {': '}
+        {body}
+      </p>
+      <p className="text-center mt-3 mb-10">
+        {'by '}
+        {name}
+      </p>
+    </li>
+  )
+}
+
+export default Comment
